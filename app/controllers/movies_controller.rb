64d18d1a94894fef7d2049efbe14e451decacf0b
EC2 Default User <ec2-user@ip-172-31-87-62.ec2.internal>
2021-02-17 12:02:27 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,28 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+
+    sort = params[:sort] || session[:sort]
+    @ratings_to_show = params[:ratings] || session[:ratings] \
+                      || Hash[@all_ratings.map { |r| [r, 1] }]
+                    
+    #case sort
+    #when 'title'
+    #  ordering, @title_cls = {:title => :asc}, 'hilite'
+    #when 'release_date'
+    #  ordering, @release_cls = {:release_date => :asc}, 'hilite'
+    #end
+
+    if !params[:commit].nil? or params[:ratings].nil? or \
+       (params[:sort].nil? && !session[:sort].nil?)
+       flash.keep
+       redirect_to movies_path :sort => sort, :ratings => @ratings_to_show
+    end
+    @movies = Movie.with_ratings(@ratings_to_show.keys)
+    #session[:sort] = sort
+    session[:ratings] = @ratings_to_show
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+      pluck(:rating).uniq
+  end
+
+  def self.with_ratings(ratings_list)
+  end
 end
